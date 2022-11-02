ce96433c03c9a6a1b60ad6462acaf923c94b2fff
seah526 <seah526@naver.com>
2022-11-03 04:09:12 +0900

    feat(frontend): test page의 code editor component 생성

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -1,81 +1,52 @@
+import React, { useState } from "react";
 import { IconButton, Button, Select, CircularProgress, CircularProgressLabel, StylesProvider } from "@chakra-ui/react";
-import { SearchIcon, ArrowDownIcon } from "@chakra-ui/icons";
-import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import { SearchIcon, DownloadIcon, CopyIcon, RepeatClockIcon } from "@chakra-ui/icons";
+import Editor from "@monaco-editor/react";
+import "../styles/style.css";
 
-const containerStyle = {
-  width: "758px",
-  height: "calc(100% - 60px)",
-  background: "#1A202C"
-};
-const navStyle = {
-  display: "flex",
-  justifyContent: "space-between",
-  height: "60px",
-  padding: "0 20px",
-  background: "green"
-};
-const navLeft = {
-  height: "inherit",
-  display: "flex",
-  flexDirection: "row",
-  gap: "10px",
-  alignItems: "center",
-};
-const navRight = {
-  height: "inherit",
-  width: "fit-content",
-  display: "flex",
-  flexDirection: "row",
-  gap: "10px",
-  alignItems: "center",
-};
-const IconBtn = {
-  width: "32px",
-  height: "32px",
-  borderRadius: "6px",
-  color: "#718096",
-};
-const select = {
-  width: "250px",
-  height: "32px",
-  borderRadius: "6px",
-};
-const HUGbtn = {
-  width: "50px",
-  height: "32px",
-  border: "none",
-  borderRadius: "6px",
-  padding: "10px 12px",
-  background: "#38A169",
+const label = {
   color: "white",
 };
 
+const progress = {
+  width: "32px",
+  height: "32px"
+};
+
 function CodeEditor() {
+  const [skeleton, setSkeleton] = useState("hello");
+
+  const inputFile = () => {
+    document.getElementById("hiddenFileInput").click();
+  };
+
   return (
-    <div className="container" style={containerStyle}>
-      <div className="nav" style={navStyle}>
-        <div className="nav-left" style={navLeft}>
-          <IconButton aria-label="Search database" icon={<SearchIcon />} style={IconBtn} />
-          <IconButton aria-label="Search" icon={<SearchIcon />} style={IconBtn} />
-          <IconButton aria-label="Search database" icon={<SearchIcon />} style={IconBtn} />
-          <IconButton aria-label="Search database" icon={<ArrowDownIcon />} style={IconBtn} />
+    <div className="container">
+      <div className="header">
+        <div className="header-item">
+          <IconButton className="iconBtn" aria-label="Search database" icon={<SearchIcon />} onClick={inputFile} />
+          <input type="file" id="hiddenFileInput" accept=".py" style={{ display: "none" }} />
+          <IconButton className="iconBtn" aria-label="Search" icon={<RepeatClockIcon />} />
+          <IconButton className="iconBtn" aria-label="Reset Code" icon={<CopyIcon />} />
+          <IconButton className="iconBtn" aria-label="Search database" icon={<DownloadIcon />} />
         </div>
-        <div className="nav-right" style={navRight}>
-          <Select placeholder="Select option" style={select}>
+        <div className="header-item">
+          <Select className="select" placeholder="Select option">
             <option value="option1">Option 1</option>
             <option value="option2">Option 2</option>
             <option value="option3">Option 3</option>
           </Select>
-          <Button style={HUGbtn}>저장</Button>
-          <CircularProgress value={40} size="32px">
-            <CircularProgressLabel>1</CircularProgressLabel>
+          <Button className="saveBtn">저장</Button>
+          <CircularProgress value={33} size="32px" style={progress}>
+            <CircularProgressLabel style={label}>1/3</CircularProgressLabel>
           </CircularProgress>
         </div>
       </div>
       <Editor
-        height="calc( 100% - 60px )"
+        height="90vh"
         defaultLanguage="python"
-        defaultValue="// skeleton code"
+        defaultValue={skeleton}
+        theme="vs-dark"
       />
     </div>
   );
