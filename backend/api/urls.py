f033ac2e1afe695075963233db149d0263cbb404
Gongmeda <gongmeda@gmail.com>
2022-11-03 02:39:28 +0900

    feat(backend): grade API 구현

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import execute
+from .views import execute, grade
 
 # api 앱 내의 url 관리
 # 추후 server의 urls에 연결
 
 urlpatterns = [
     path("execute/", execute),
+    path("problems/<int:problem_id>/grade/<int:testcase_num>/", grade)
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,18 +6,16 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 import api.executor as executor
+from api.models import Problem
 from server.settings.base import BASE_DIR
 
 
 @api_view(["POST"])
 def execute(request: Request):
-    data = request.data
+    code = request.data.get("code")
+    inp = request.data.get("input")
 
-    code = data.get("code")
-    inp = data.get("input")
-
-    file_name = f"{uuid.uuid4()}.py"
-    file_path = BASE_DIR.joinpath("tmp", file_name)
+    file_path = BASE_DIR.joinpath("tmp", f"{uuid.uuid4()}.py")
 
     # create directory if not exists
     if not file_path.parent.exists():
@@ -32,4 +30,54 @@ def execute(request: Request):
     # remove file
     remove(file_path)
 
-    return Response({"result": outp, "error": err})
+    return Response(
+        {
+            "result": outp,
+            "error": err
+        }
+    )
+
+
+@api_view(["POST"])
+def grade(request: Request, problem_id: int, testcase_num: int):
+    code = request.data.get("code")
+    problem = Problem.objects.get(id=problem_id)  # TODO: Not Found Exception 처리
+    testcase: dict = problem.testcases[testcase_num - 1]  # TODO: IndexError 처리
+
+    tc_inp = testcase.get("input")
+    tc_outp = testcase.get("output")
+    timeout = testcase.get("timeout")
+    is_hidden = testcase.get("is_hidden")
+
+    file_path = BASE_DIR.joinpath("tmp", f"{uuid.uuid4()}.py")
+
+    # create directory if not exists
+    if not file_path.parent.exists():
+        file_path.parent.mkdir()
+
+    # write code to file
+    with open(file_path, "w") as f:
+        f.write(code)
+
+    outp, err = executor.run(file_path, code, tc_inp, timeout)
+
+    is_passed = str(outp).strip() == str(tc_outp).strip()
+
+    if is_hidden:
+        tc_inp = None
+        tc_outp = None
+        outp = None
+        err = None
+
+    # remove file
+    remove(file_path)
+
+    return Response(
+        {
+            "is_passed": is_passed,
+            "input": tc_inp,
+            "output": tc_outp,
+            "result": outp,
+            "error": err
+        }
+    )
