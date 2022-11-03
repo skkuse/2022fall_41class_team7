ead728905cda4d64ebd8f9a7e1f4de76a73ec3c3
Gongmeda <gongmeda@gmail.com>
2022-11-03 20:07:34 +0900

    fix(backend): login API 구현 수정

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,59 +4,40 @@ from os import remove
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
 
 import api.executor as executor
-from api.models import Problem
 from server.settings.base import BASE_DIR
-
-from django.http import HttpResponse
-from django.http.response import JsonResponse
-
-from .models import *
 from .serializers import *
 
-@api_view(["POST"])
-def loginAPI(request):
-    login_data = JSONParser().parse(request)
-    search_id = login_data['student_id']
-    
-    try:
-        user = User.objects.get(student_id = search_id)
-    except User.DoesNotExist:
-        return HttpResponse(status = 404)
-    
-    if login_data['password'] == user.password:
-        user_serializer = MiniUserSerializer(user)
-        return JsonResponse(user_serializer.data, status = 200, safe = False)
-    else:
-        return HttpResponse(status = 401)
-    
-    """ Find out why authenticat is not working
-    
-    login_data = JSONParser().parse(request)
-    id = login_data['student_id']
-    pw = login_data['password']
-    
-    result = authenticate(username = id, password = pw)
-    if result:
-        return HttpResponse(status=200)
-    else:
-        return HttpResponse(status=401)
-    """
 
-    
-@api_view(["GET"])    
-def problemAPI(request, id = 0):
-    
-    if id == 0: # '/question/'
+@api_view(["POST"])
+def loginAPI(request: Request):
+    student_id = request.data.get("student_id")
+    password = request.data.get("password")
+
+    try:
+        user = User.objects.get(student_id=student_id)
+    except User.DoesNotExist:
+        return Response(status=404)
+
+    if password == user.password:
+        user_dict = UserSerializer(user).data
+        user_dict["password"] = None
+        return Response(user_dict, status=200)
+    else:
+        return Response(status=401)
+
+
+@api_view(["GET"])
+def problemAPI(request, id=0):
+    if id == 0:  # '/question/'
         problem = Problem.objects.all()
-    else:       # '/question/<id>/'
-        problem = Problem.objects.filter(id = id)
-    
+    else:  # '/question/<id>/'
+        problem = Problem.objects.filter(id=id)
+
     # how to remove hidden case
-    problem_serializer = MiniProblemSerializer(problem, many = True)
-    return JsonResponse(problem_serializer.data, status = 200, safe = False)
+    problem_serializer = MiniProblemSerializer(problem, many=True)
+    return JsonResponse(problem_serializer.data, status=200, safe=False)
 
 
 @api_view(["POST"])
