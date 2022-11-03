b956e9cdd112acdf8d6707eef83117c15eb79751
Kim Ho Jin <borussen@Naver.com>
2022-11-03 13:54:47 +0900

    feat(backend): login API 구현

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,6 +8,20 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "student_id", "password")
 
+class MiniUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','student_id')
+
+class ProblemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Problem
+        fields = ('id', 'class_id', 'explanation', 'reference', 'testcases', 'skeleton_code', 'answer_code', 'related_content')
+
+class MiniProblemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Problem
+        fields = ('id', 'class_id', 'explanation', 'reference', 'testcases', 'skeleton_code')
 
 class ExecuteSerializer(serializers.Serializer):
     input = serializers.CharField(required=False)
