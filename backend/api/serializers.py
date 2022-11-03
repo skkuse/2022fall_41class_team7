3c5621874812f2ae4a3672a6c2c6bd016b9df848
Kim Ho Jin <borussen@Naver.com>
2022-11-03 21:03:12 +0900

    refactor(backend): login, problem api refactoring

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,20 +8,21 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "student_id", "password")
 
-class MiniUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id','student_id')
 
 class ProblemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Problem
-        fields = ('id', 'class_id', 'explanation', 'reference', 'testcases', 'skeleton_code', 'answer_code', 'related_content')
+        fields = (
+            "id",
+            "class_id",
+            "explanation",
+            "reference",
+            "testcases",
+            "skeleton_code",
+            "answer_code",
+            "related_content",
+        )
 
-class MiniProblemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Problem
-        fields = ('id', 'class_id', 'explanation', 'reference', 'testcases', 'skeleton_code')
 
 class ExecuteSerializer(serializers.Serializer):
     input = serializers.CharField(required=False)
