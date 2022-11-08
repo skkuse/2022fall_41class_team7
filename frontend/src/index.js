699163f4a8c7b8fcd09b362615251fe939e8d8c3
Gongmeda <gongmeda@gmail.com>
2022-11-08 14:45:01 +0900

    chore(frontend): Home 컴포넌트 pages로 이동

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
