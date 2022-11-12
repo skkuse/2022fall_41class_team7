b2abb444c1be8fbc84d2b72163f5a8cfd4d8ab6b
KimYangSeon <endol_@naver.com>
2022-11-12 20:57:48 +0900

    feat(frontend): 홈페이지 로그인 api 연동

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Home from "./pages/Home";
+import App from "./pages/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
