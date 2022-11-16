935a334dc78e2dc1e7674d2dd7de966e09b9efd6
Gongmeda <gongmeda@gmail.com>
2022-11-17 02:39:55 +0900

    refactor(backend): auth API 마무리

--- backend/api/serializers/auth_serializers.py
+++ backend/api/serializers/auth_serializers.py
@@ -4,6 +4,10 @@ from api.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    student_id = serializers.CharField(max_length=10, validators=[])
+
     class Meta:
         model = User
-        fields = ("id", "student_id", "password")
+        fields = ("id", "student_id", "name", "password")
+        read_only_fields = ("name",)
+        extra_kwargs = {"password": {"write_only": True}}
--- backend/api/views/auth_views.py
+++ backend/api/views/auth_views.py
@@ -7,30 +7,35 @@ from rest_framework.decorators import (
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from api.serializers import UserSerializer
+
 
 @api_view(["POST"])
 @authentication_classes([])
 @permission_classes([])
 def login(request: Request):
-    student_id = request.data.get("student_id")
-    password = request.data.get("password")
+    user_serializer = UserSerializer(data=request.data)
+    user_serializer.is_valid(raise_exception=True)
+
+    student_id = user_serializer.validated_data.get("student_id")
+    password = user_serializer.validated_data.get("password")
 
     # 요청 validation
     if student_id is None or password is None:
-        return Response({"error": "student_id or password is not given"}, status=400)
+        return Response({"error": "학번과 비밀번호를 모두 적어 주세요."}, status=400)
 
     # 유저 정보 검증
     user = authenticate(student_id=student_id, password=password)
     if user is None:
-        return Response({"error": "student_id or password is wrong"}, status=400)
+        return Response({"error": "학번 또는 비밀번호가 올바르지 않습니다."}, status=400)
 
     # 로그인 처리
     auth_login(request, user)
-    return Response({"id": user.id, "student_id": user.student_id}, status=200)
+    return Response(UserSerializer(user).data, status=200)
 
 
 @api_view(["GET"])
 def logout(request: Request):
     # 로그아웃 처리
     auth_logout(request)
-    return Response({"success": "logout success"}, status=200)
+    return Response(status=200)
