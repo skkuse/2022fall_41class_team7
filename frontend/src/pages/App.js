1a4a696d4d6c25e50471331cd4a5eea9d5e510dc
Lee Joo Bin <evenoaarenim@gmail.com>
2022-11-17 16:47:32 +0900

    hover css & scroll lock
    add hover and press & fix x,y scroll

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,4 +1,5 @@
 import "../styles/base.css";
+import "../styles/hover.css";
 import { ChakraProvider, Box, Divider, Button } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import axios from "axios";
