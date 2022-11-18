524bc2b3ba766e00a9ecdf46d2abd1fd9852fc1f
Gongmeda <gongmeda@gmail.com>
2022-11-18 12:11:57 +0900

    feat(backend): file interceptor 구현

--- backend/api/common/__init__.py
+++ backend/api/common/__init__.py
@@ -1,1 +1,2 @@
 from .executor import *
+from .file_interceptor import *
--- backend/api/views/submission_views.py
+++ backend/api/views/submission_views.py
@@ -1,5 +1,4 @@
-import uuid
-from os import remove
+from typing import TextIO
 
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import ValidationError
@@ -7,7 +6,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from api.common import executor
+from api.common import executor, file_interceptor
 from api.models import Problem
 from api.serializers import (
     CodeSerializer,
@@ -16,30 +15,21 @@ from api.serializers import (
     GradeQueryParamsSerializer,
     ExecuteSerializer,
 )
-from server.settings.base import BASE_DIR
 
 
 @api_view(["POST"])
-def execute(request: Request):
+@file_interceptor
+def execute(request: Request, file: TextIO):
     execute_serializer = ExecuteSerializer(data=request.data)
     execute_serializer.is_valid(raise_exception=True)
     code = execute_serializer.validated_data.get("code")
     user_in = execute_serializer.validated_data.get("input")
 
-    file_path = BASE_DIR.joinpath("tmp", f"{uuid.uuid4()}.py")
-
-    # create directory if not exists
-    if not file_path.parent.exists():
-        file_path.parent.mkdir()
-
     # write code to file
-    with open(file_path, "w") as f:
-        f.write(code)
+    file.write(code)
+    file.close()
 
-    user_out, err = executor.run(file_path, code, user_in)
-
-    # remove file
-    remove(file_path)
+    user_out, err = executor.run(file.name, code, user_in)
 
     result_serializer = ExecuteResultSerializer(data={"result": user_out, "error": err})
     result_serializer.is_valid(raise_exception=True)
@@ -48,7 +38,8 @@ def execute(request: Request):
 
 
 @api_view(["POST"])
-def grade(request: Request):
+@file_interceptor
+def grade(request: Request, file: TextIO):
     query_params_serializer = GradeQueryParamsSerializer(data=request.query_params)
     query_params_serializer.is_valid(raise_exception=True)
     problem_id = query_params_serializer.validated_data.get("problem_id")
@@ -69,22 +60,13 @@ def grade(request: Request):
     timeout = testcase.get("timeout")
     is_hidden = testcase.get("is_hidden")
 
-    file_path = BASE_DIR.joinpath("tmp", f"{uuid.uuid4()}.py")
-
-    # create directory if not exists
-    if not file_path.parent.exists():
-        file_path.parent.mkdir()
-
     # write code to file
-    with open(file_path, "w") as f:
-        f.write(code)
+    file.write(code)
+    file.close()
 
-    user_out, err = executor.run(file_path, code, tc_in, timeout)
+    user_out, err = executor.run(file.name, code, tc_in, timeout)
 
     is_passed = str(user_out).strip() == str(tc_out).strip()
-
-    # remove file
-    remove(file_path)
 
     response_serializer = GradeResultSerializer(
         data={
