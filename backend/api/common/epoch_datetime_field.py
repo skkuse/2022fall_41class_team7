a1b75133f1d421011eb759f2eae54a2e7d53a827
Gongmeda <gongmeda@gmail.com>
2022-11-22 14:01:21 +0900

    fix(backend): EpochDateTimeField 기본으로 required=False 설정 추가

--- backend/api/common/epoch_datetime_field.py
+++ backend/api/common/epoch_datetime_field.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 
 
 class EpochDateTimeField(serializers.DateTimeField):
+    def __init__(self, format=None, input_formats=None, default_timezone=None, **kwargs):
+        kwargs["required"] = kwargs.get("required", False)
+        super().__init__(format, input_formats, default_timezone, **kwargs)
+
     def to_representation(self, value):
         """Return epoch time for a datetime object or ``None``"""
         import time
