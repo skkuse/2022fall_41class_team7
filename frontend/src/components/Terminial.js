e32c82169bca459eb5e2fdbdad1f5e63b189120b
seah526 <seah526@naver.com>
2022-11-25 20:12:29 +0900

    feat(front): Run Code modal 생성 및 실행 btn에 연결

--- frontend/src/components/Terminial.js
+++ frontend/src/components/Terminial.js
@@ -1,4 +1,5 @@
-import { Box, Button, CircularProgress, CircularProgressLabel, Text } from "@chakra-ui/react";
+import { Box, Button, CircularProgress, CircularProgressLabel, Text, useDisclosure } from "@chakra-ui/react";
+import RunCode from "./modals/RunCode";
 
 const progress = {
   width: "32px",
@@ -6,11 +7,19 @@ const progress = {
 };
 
 function Terminal() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box className="terminal_container">
       <Box className="terminal_header">
         <Box className="terminal_btn_container">
-          <Button size="sm" backgroundColor="gray.500">
+          <Button
+            size="sm"
+            backgroundColor="gray.500"
+            onClick={() => {
+              onOpen();
+            }}
+          >
             실행
           </Button>
           <Button size="sm" backgroundColor="gray.500">
@@ -28,6 +37,10 @@ function Terminal() {
         <Text fontSize={14}>22:24:08 {">>"} 프로세스가 시작되었습니다.</Text>
         <Text fontSize={14}>22:24:09 {">>"} 처리중...</Text>
       </Box>
+      <RunCode
+        isOpen={isOpen}
+        onClose={onClose}
+      />
     </Box>
   );
 }
