c7948fd3924c2b28efb089f66ec35fdac2929dd0
Kim Ho Jin <borussen@Naver.com>
2022-11-27 00:42:33 +0900

    feat(front): logout API 연결

--- frontend/src/components/modals/Logout.js
+++ frontend/src/components/modals/Logout.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import {
   Modal,
   ModalOverlay,
@@ -9,10 +10,30 @@ import {
   Button,
   Text,
 } from "@chakra-ui/react";
+import axios from "axios";
 
 function Logout({ isOpen, onClose }) {
+  axios.defaults.withCredentials = true;
+  axios.defaults.xsrfCookieName = "csrftoken";
+  axios.defaults.xsrfHeaderName = "X-CSRFToken";
+
   const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
+  const [isFailed, setIsFailed] = useState(false);
+
+  const logout = () => {
+    axios
+      .get(
+        "/api/logout/",
+        { headers: { "Content-Type": "application/json", authentification: "X-CSRFToken" } }
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          setIsFailed(false);
+        }
+      })
+      .catch((error) => setIsFailed(true));
+  };
 
   return (
     <Modal
@@ -23,17 +44,19 @@ function Logout({ isOpen, onClose }) {
       isCentered
     >
       <ModalOverlay />
-      <ModalContent>
-        <ModalBody pb={6}>
-          <Text>홈페이지로 이동하겠습니까?</Text>
-          <Text>(계정은 로그아웃됩니다)</Text>
+      <ModalContent backgroundColor="#1A202C" width="370px" height="170px">
+        <ModalBody pb={6} color="white" textAlign="center">
+          <Text fontSize="20px" marginTop="10px">홈페이지로 이동하겠습니까?</Text>
+          <Text fontSize="16px" marginTop="5px">계정은 로그아웃됩니다</Text>
         </ModalBody>
 
         <ModalFooter>
-          <Button onClick={onClose}>취소</Button>
-          <Button colorScheme="blue" mr={3}>
-            로그아웃
-          </Button>
+          <Button onClick={onClose} marginRight="10px">취소</Button>
+          <Link to="/">
+            <Button colorScheme="blue" mr={3} onClick={logout}>
+              로그아웃
+            </Button>
+          </Link>
         </ModalFooter>
       </ModalContent>
     </Modal>
