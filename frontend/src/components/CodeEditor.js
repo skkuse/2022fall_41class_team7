195d248eae9e437c09b7f5e7165c98345a972b01
Lee Joo Bin <evenoaarenim@gmail.com>
2022-11-26 20:56:06 +0900

    스크롤 변경 / 실행버튼 작동
    Terminal 수정
    CodeEditor 수정
    RunCode 재작성

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -75,6 +75,10 @@ function CodeEditor({ storageCapacity, storages, skeletonCode }) {
       duration: 1000,
     });
   };
+  
+  const onChangeEditor = () => {
+    document.getElementById("hiddenCodeValue").value = editorRef.current?.getValue();
+  };
 
   const getUpdatedTime = (updatedAt) => {
     const updatedime = new Date(updatedAt);
@@ -184,7 +188,11 @@ function CodeEditor({ storageCapacity, storages, skeletonCode }) {
         defaultValue={value}
         theme="vs-dark"
         onMount={handleEditorDidMount}
+        onChange={onChangeEditor}
       />
+      
+      <input type="hidden" id="hiddenCodeValue" value={value} />
+
     </Box>
   );
 }
