f1f2e3ecd1222ceedd1c14cd97ac23aac456dcfc
KimYangSeon <endol_@naver.com>
2022-11-30 22:46:11 +0900

    fix(front): 테스트케이스 검증 toast 수정

--- frontend/src/components/Testcase.js
+++ frontend/src/components/Testcase.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { ChakraProvider, Box, Button, Text, useToast } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "../utils/axios";
 import PassTag from "./PassTag";
 import FailTag from "./FailTag";
@@ -14,7 +14,14 @@ function Testcase({ problemID, number, title, input, output }) {
   const getCode = () => document.getElementById("hiddenCodeValue").value;
 
   const validate = async () => {
-    await axios
+    toast({
+      title: "테스트케이스 검증을 진행합니다.",
+      position: "bottom-right",
+      isClosable: true,
+      duration: 1000,
+    });
+
+    const response = await axios
       .post(
         "grade/",
         { code: getCode() },
@@ -25,16 +32,6 @@ function Testcase({ problemID, number, title, input, output }) {
           },
         }
       )
-      .then((response) => {
-        setTestcase(response.data);
-        toast({
-          title: "테스트케이스 검증을 진행합니다.",
-          position: "bottom-right",
-          isClosable: true,
-          status: "success",
-          duration: 1000,
-        });
-      })
       .catch((error) => {
         toast({
           title: "테스트케이스 검증에 실패했습니다.",
@@ -44,6 +41,8 @@ function Testcase({ problemID, number, title, input, output }) {
           duration: 1000,
         });
       });
+
+    setTestcase(response.data);
   };
 
   const checkValidate = () => {
