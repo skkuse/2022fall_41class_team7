72a52f9000b50051256cfcb6190c2593eb5994e7
YunTae <67727745+yuntaeJ@users.noreply.github.com>
2022-12-11 01:39:08 +0900

    Update tasks.py

--- backend/api/common/tasks.py
+++ backend/api/common/tasks.py
@@ -30,9 +30,9 @@ def grade_submission(submission_id: int, file: TextIO):
         tc_out = testcase.get("output")
         timeout = testcase.get("timeout")
 
-        user_out, err = executor.run(file.name, tc_in, timeout)
+        user_out, err, err_line = executor.run(file.name, tc_in, timeout)
 
-        is_passed = str(user_out.strip()) == str(tc_out.strip())
+        is_passed = str(user_out.strip()) == str(tc_out.strip()) if user_out else False
         result.append(
             {
                 "input": tc_in,
@@ -52,7 +52,7 @@ def grade_submission(submission_id: int, file: TextIO):
 
 @shared_task
 @file_interceptor()
-def analyze_submission(submission_id: int, file: TextIO, file_answer: TextIO):
+def analyze_submission(submission_id: int, file: TextIO):
     # get submission
     submission = get_object_or_404(
         Submission.objects.filter(id=submission_id).select_related("problem")
@@ -70,12 +70,14 @@ def analyze_submission(submission_id: int, file: TextIO, file_answer: TextIO):
     file.write(submission.code)
     file.close()
 
-    # write answercode to file
-    file_answer.write(problem.answer_code)
-    file_answer.close()
+    # Open a new file in write mode
+    answer_filename = "answer.py"
+    with open(answer_filename, "w") as f:
+        # Write to the file
+        f.write(problem.answer_code)
 
     # 표절 검사
-    plagiarism = execute_plagiarism(file.name)
+    plagiarism = execute_plagiarism(file.name, answer_filename)
 
     # 가독성 채점
     readability = {}
@@ -83,10 +85,9 @@ def analyze_submission(submission_id: int, file: TextIO, file_answer: TextIO):
     readability["pylint"] = execute_pylint(file.name)
     readability["eradicate"] = execute_eradicate(file.name)
     readability["radon"] = execute_radon(file.name)
-    readability["eradicate"] = execute_eradicate(file.name)
+    readability["pycodestyle"] = execute_pycodestyle(file.name)
 
-    # 효율 채점
-    efficiency = execute_efficiency(file.name, file_answer.name)
+    efficiency = execute_efficiency(file.name, answer_filename)
 
     # 코드 설명
     explanation = execute_codex(file.name)
