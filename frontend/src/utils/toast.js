f2ffd1c065bf03ce2f8e48f9d638fd99f0f12fcd
KimYangSeon <endol_@naver.com>
2022-12-12 15:25:46 +0900

    fix(front): lint

--- frontend/src/utils/toast.js
+++ frontend/src/utils/toast.js
@@ -1,6 +1,7 @@
 import { useToast as useChakraToast } from "@chakra-ui/react";
 
-const useToast = () =>
+const useToast = () => {
   useChakraToast({ position: "bottom-right", isClosable: true, duration: 2000 });
+};
 
 export default useToast;
