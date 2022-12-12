94b279d43ff77edae1a63cd1d9798f6f291f1be9
KimYangSeon <endol_@naver.com>
2022-12-12 15:54:12 +0900

    fix(front): 토스트 lint 취소

--- frontend/src/utils/toast.js
+++ frontend/src/utils/toast.js
@@ -1,7 +1,6 @@
 import { useToast as useChakraToast } from "@chakra-ui/react";
 
-const useToast = () => {
+const useToast = () =>
   useChakraToast({ position: "bottom-right", isClosable: true, duration: 2000 });
-};
 
 export default useToast;
